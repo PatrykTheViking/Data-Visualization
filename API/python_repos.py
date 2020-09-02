1f63fe7a8e1aaf8e61f3e0a8103036611060fc4a
Patryk <patryk.bytow@wp.pl>
2020-09-02 13:25:10 +0200

    Python repos update

--- API/python_repos.py
+++ API/python_repos.py
@@ -8,4 +8,19 @@ print("Code status: {}".format(r.status_code))
 
 response_dict = r.json()
 
-print(response_dict.keys())
+print("Total number of repos: {}".format(response_dict['total_count']))
+
+repo_dicts = response_dict['items']
+print("Number of returned repos: {}".format(len(repo_dicts)))
+
+repo_dict = repo_dicts[0]
+# print("\nKeys: {}".format(len(repo_dict)))
+# for key in sorted(repo_dict.keys()):
+#     print(key)
+for repo_dict in repo_dicts:
+    print("\nSelected info about first repo: ")
+    print("Name: {}".format(repo_dict['name']))
+    print("Owner: {}".format(repo_dict['owner']['login']))
+    print("Mark: {}".format(repo_dict['stargazers_count']))
+    print("Repo: {}".format(repo_dict['html_url']))
+    print("Description: {}".format(repo_dict['description']))
